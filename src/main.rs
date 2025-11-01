9f7781913a9056fd549e383061326f724cbef1a5
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2025-11-01 13:37:07 +0530

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+fn main() -> io::Result<()> {
+    loop {
+        print!("$ ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?; 
+        input = input.trim().into();
+        println!("{input}: command not found");
+    }
 }
